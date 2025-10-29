6429d6236f0422db8f07cd70f9e2662b588db59c
Denis Malorodov <denis.malod@mail.ru>
2025-10-29 14:29:11 +1000

    feat: Добавить админку

--- cashflow/finance/admin.py
+++ cashflow/finance/admin.py
@@ -1,31 +1,36 @@
 from django.contrib import admin
 from .models import Status, Type, Category, SubCategory, CashFlowRecord
 
+
 @admin.register(Status)
 class StatusAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('id', 'name')
     search_fields = ('name',)
+
 
 @admin.register(Type)
 class TypeAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('id', 'name')
     search_fields = ('name',)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type',)
+    list_display = ('id', 'name', 'type')
     list_filter = ('type',)
     search_fields = ('name',)
 
+
 @admin.register(SubCategory)
 class SubCategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category',)
+    list_display = ('id', 'name', 'category')
     list_filter = ('category',)
     search_fields = ('name',)
 
+
 @admin.register(CashFlowRecord)
 class CashFlowRecordAdmin(admin.ModelAdmin):
-    list_display = ('date', 'amount', 'type', 'category', 'subcategory', 'status',)
-    list_filter = ('date', 'type', 'category', 'status',)
+    list_display = ('id', 'date', 'amount', 'type', 'category', 'subcategory', 'status', 'comment')
+    list_filter = ('date', 'type', 'category', 'status')
     search_fields = ('comment',)
     date_hierarchy = 'date'
